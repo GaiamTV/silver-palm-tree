b94314377bf86375410b73e657f756bd924403f9
jelyware <jely2159@colorado.edu>
2019-12-21 00:10:05 -0700

    initial Tile component #1
    initial creation and integration of Tile component.. image displays but does not yet meet all UI requirements

--- src/Tile.js
+++ src/Tile.js
@@ -2,8 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getVideoRequest} from './actions/video-actions'
 import './Tile.css';
+import tileImgMock from './images/junior_front_end_ui_mock.png'
+import tileImgSimple from './images/Simple_Tile.png'
 
 class Tile extends Component {
-  someFunc () {
-    //does something
+  constructor() {
+    super();
+    this.state = {
+      tile: []
+    }
   }
+
+  render(){
+    return(
+      <img src={tileImgSimple}/>
+    )
+  }
+}
+
+export default Tile;

7328f1f569d781c45270acfa1aeaa0b7b8a2fb43
jelyware <jely2159@colorado.edu>
2019-12-20 23:25:31 -0700

    Added placeholders for Tile component

---
+++ src/Tile.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getVideoRequest} from './actions/video-actions'
+import './Tile.css';
+
+class Tile extends Component {
+  someFunc () {
+    //does something
+  }
